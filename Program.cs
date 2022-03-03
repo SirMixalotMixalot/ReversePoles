45bbd7246a99937b48fa54cca7c6c07a08e56922
Bk the ran man <Bigbk75@gmail.com>
2022-03-03 11:57:04 +0200

    Tried parentheses

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@ namespace ReversePoles
 {
     public static class Poles
     {
+        static List<Token> TokenizeParens(string expression) {
+            int parenLevel = 0;
+            var tokens = new List<Token>();
+            foreach(string s in expression.Trim().Split(" ")) {
+                if (s == "(" || s == ")") {
+                    parenLevel += s == ")" ? 1: -1;
+                    continue;
+                }
+                var token = Utilities.Tokens.GetValueOrDefault(s[0], Utilities.TokenFromNumber(s));
+                token.precedence += 2 * parenLevel;
+                tokens.Add(token);
+            }
+            return tokens;
+
+        }
+
         static IEnumerable<Token> Tokenize(string expression)
         {
             return expression.Trim()
@@ -14,8 +30,8 @@ namespace ReversePoles
 
         public static void Main()
         {
-            const string expression = "5 + 5 - 6 * 2";
-            var tokens = Tokenize(expression).ToArray();
+            const string expression = "(5 + 4)/(2 - 1)";
+            var tokens = TokenizeParens(expression).ToArray();
             var tree = new ExpressionTree(tokens);
 
             foreach(var token in tree.PostOrderTraversal()) {
